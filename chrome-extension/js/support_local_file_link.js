22a923ab7255bbb120ad64e483c161bfdbc094a6
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-03-09 18:13:44 +0900

    [Clean] 文字列がファイルサーバーのパスかどうかの判定とfileUrlへの変化ロジックを整理

--- chrome-extension/js/support_local_file_link.js
+++ chrome-extension/js/support_local_file_link.js
@@ -1,5 +1,29 @@
 {
 	const alreadyCheckKey = `data-already_local_file_link_checked-${chrome.runtime.id}`;
+	const filePathChecker = (() => {
+		const rules = [];
+		return {
+			addRule: rule => {
+				rules.push(rule);
+			},
+			checkAndGetFuleUrl: maybePathString => {
+				const matchedRule = rules.find(rule => {
+					return rule.isFilePath(maybePathString)
+				});
+				if (!matchedRule) return null;
+				return matchedRule.generateFileUrl(maybePathString);
+			}
+		};
+	})();
+	filePathChecker.addRule({
+		isFilePath: str => str.startsWith("\\\\"),
+		generateFileUrl: str => "file:" + str.replace(/\\/g, "/")
+	});
+	// ダブルクオーテーションで囲まれている時
+	filePathChecker.addRule({
+		isFilePath: str => str.startsWith('"\\\\') && str.endsWith('"'),
+		generateFileUrl: str => "file:" + str.slice(1, -1).replace(/\\/g, "/")
+	});
 	document.body.addEventListener("mouseover", evt => {
 		const target = evt.target;
 		if (!target.classList.contains("message_body")) return;
@@ -9,14 +33,9 @@
 			return node instanceof Text;
 		}).forEach(textNode => {
 			const text = textNode.textContent;
-			if (text.startsWith("\\\\")) {
-				const url = "file:" + text.replace(/\\/g, "/");
-				const a = document.createElement("a");
-				a.href = url;
-				a.innerText = text;
-				target.replaceChild(a, textNode);
-			} else if (text.endsWith('"') && text.startsWith('"\\\\')) {
-				const url = "file:" + text.slice(1, -1).replace(/\\/g, "/");
+			const maybeFileUrl = filePathChecker.checkAndGetFuleUrl(text);
+			if (maybeFileUrl !== null) {
+				const url = maybeFileUrl;
 				const a = document.createElement("a");
 				a.href = url;
 				a.innerText = text;
@@ -27,14 +46,9 @@
 			return node.tagName === "CODE";
 		}).forEach(codeNode => {
 			const text = codeNode.textContent;
-			if (text.startsWith("\\\\")) {
-				const url = "file:" + text.replace(/\\/g, "/");
-				const a = document.createElement("a");
-				a.href = url;
-				target.replaceChild(a, codeNode);
-				a.appendChild(codeNode);
-			} else if (text.endsWith('"') && text.startsWith('"\\\\')) {
-				const url = "file:" + text.slice(1, -1).replace(/\\/g, "/");
+			const maybeFileUrl = filePathChecker.checkAndGetFuleUrl(text);
+			if (maybeFileUrl !== null) {
+				const url = maybeFileUrl;
 				const a = document.createElement("a");
 				a.href = url;
 				target.replaceChild(a, codeNode);
